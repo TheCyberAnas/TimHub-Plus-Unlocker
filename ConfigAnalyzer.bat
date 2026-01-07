951fbba156c1d84ce6ccf5b6952c192cddab6093
CyberAnas <253500177+TheCyberAnas@users.noreply.github.com>
2026-01-07 18:15:05 +0100

    Create ConfigAnalyzer.bat

---
+++ ConfigAnalyzer.bat
@@ -0,0 +1,19 @@
+@echo off
+title CONFIG ANALYZER - @thecyberanas
+color 0d
+cls
+echo ==================================================
+echo         CONFIG BIN ANALYZER BY THECYBERANAS
+echo ==================================================
+echo.
+echo [*] Analisi intestazione file config.bin...
+echo.
+echo [+] Prodotto: MediaAccess
+echo [+] Firmware rilevato: AGTHF_1.0.1
+echo [+] Stato Criptazione: AES-CBC (CIPHERKEY=GW)
+echo.
+echo --------------------------------------------------
+echo [!] NOTA: Il firmware 1.0.1 e' vulnerabile!
+echo Prossimo passo: Decriptazione payload e sblocco SSH.
+echo --------------------------------------------------
+pause
